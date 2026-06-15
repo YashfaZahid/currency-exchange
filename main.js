38b0ac5cbd0b929d711593d783f3e67430ce326b
Yashfa Zahid <yashfa229@gmail.com>
2026-06-15 23:19:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const api_key="6ceaff8ff3e742fbdab53f9f"
+base_url=`https://v6.exchangerate-api.com/v6/${api_key}/pair`
+
+async function handleClick(){
+    const baseAmount=document.getElementsByName("base-amount")[0]
+    const baseCurrency=document.getElementsByName("Base-Currency")[0]
+    const targetCurrency=document.getElementsByName("Target-Currency")[0]
+    const baseValue=baseCurrency.value
+    const targetValue=targetCurrency.value
+    const baseAmountValue=baseAmount.value
+    console.log(baseValue)
+    console.log(targetValue)
+    try{
+    const response=await fetch(`${base_url}/${baseValue}/${targetValue}/`)
+    if (!response.ok) {
+      throw new Error(`Status: ${response.status}`);
+    }
+
+    const data = await response.json(); 
+    console.log('Data:', data);
+    let result=document.getElementById("result")
+    result.innerHTML=`${baseAmountValue} ${baseValue} = ${Math.round(data.conversion_rate*baseAmountValue)} ${targetValue}`
+    
+  } catch (error) {
+    console.error(error.message);
+  }    
+
+}
+
